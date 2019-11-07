196f0935f8f60c7477e6510d60253892ebd7bf57
songjialin <songjialin@50lion.com>
2019-11-07 17:38:06 +0800

    init

--- backend/src/main/java/com/github/yungyu16/gobang/config/WebConfig.java
+++ backend/src/main/java/com/github/yungyu16/gobang/config/WebConfig.java
@@ -5,17 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
 
     @Bean
     public CorsFilter corsFilter() {
