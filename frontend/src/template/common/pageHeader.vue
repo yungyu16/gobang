f1097db436b8a0e6013d79d4bce6c04d64d5ebe1
songjialin <songjialin@50lion.com>
2019-11-07 18:36:09 +0800

    init

--- frontend/src/template/common/pageHeader.vue
+++ frontend/src/template/common/pageHeader.vue
@@ -3,10 +3,11 @@
              @select="handleSelect"
              active-text-color="#ffd04b"
              background-color="#545c64"
+             router
              mode="horizontal"
              text-color="#fff">
-        <el-menu-item index="1">大厅</el-menu-item>
-        <el-menu-item index="2">对局</el-menu-item>
+        <el-menu-item :route='{path:"/hall"}' index="1">大厅</el-menu-item>
+        <el-menu-item :route='{path:"/game"}' index="2">对局</el-menu-item>
         <el-menu-item disabled index="3">消息中心</el-menu-item>
     </el-menu>
 </template>
--- frontend/src/template/page/hall.vue
+++ frontend/src/template/page/hall.vue
@@ -1,19 +1,133 @@
 <template>
-    <span>hellofsdfsdf</span>
+    <el-row>
+        <el-col :span="8">
+            <el-card class="box-card">
+                <div class="clearfix" slot="header">
+                    <span>在线用户</span>
+                </div>
+                <el-table :data="tableData"
+                          height="600px"
+                          style="width: 100%">
+                    <el-table-column
+                            label="用户名"
+                            prop="date"
+                            width="180">
+                    </el-table-column>
+                    <el-table-column
+                            label="积分"
+                            prop="name"
+                            width="180">
+                    </el-table-column>
+                    <el-table-column
+                            label="胜"
+                            prop="address">
+                    </el-table-column>
+                    <el-table-column
+                            label="负"
+                            prop="address">
+                    </el-table-column>
+                    <el-table-column
+                            label="操作"
+                            prop="address">
+                    </el-table-column>
+                </el-table>
+            </el-card>
+        </el-col>
+        <el-col :span="8">
+            <el-card class="box-card">
+                <div class="clearfix" slot="header">
+                    <span>在线对局室</span>
+                </div>
+                <el-table :data="tableData"
+                          height="600px"
+                          style="width: 100%">
+                    <el-table-column
+                            label="用户名"
+                            prop="date"
+                            width="180">
+                    </el-table-column>
+                    <el-table-column
+                            label="积分"
+                            prop="name"
+                            width="180">
+                    </el-table-column>
+                    <el-table-column
+                            label="胜"
+                            prop="address">
+                    </el-table-column>
+                    <el-table-column
+                            label="负"
+                            prop="address">
+                    </el-table-column>
+                    <el-table-column
+                            label="操作"
+                            prop="address">
+                    </el-table-column>
+                </el-table>
+            </el-card>
+        </el-col>
+        <el-col :span="8">
+            <el-card class="box-card">
+                <div class="clearfix" slot="header">
+                    <span>聊天室</span>
+                </div>
+                <el-table :data="tableData"
+                          style="width: 100%">
+                    <el-table-column
+                            label="用户名"
+                            prop="date"
+                            width="180">
+                    </el-table-column>
+                    <el-table-column
+                            label="积分"
+                            prop="name"
+                            width="180">
+                    </el-table-column>
+                    <el-table-column
+                            label="胜"
+                            prop="address">
+                    </el-table-column>
+                    <el-table-column
+                            label="负"
+                            prop="address">
+                    </el-table-column>
+                    <el-table-column
+                            label="操作"
+                            prop="address">
+                    </el-table-column>
+                </el-table>
+            </el-card>
+        </el-col>
+    </el-row>
 </template>
-
 <script>
     export default {
         name: "hall.vue",
-        date() {
-            return {}
+        data() {
+            return {
+                tableData: [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }]
+            }
         },
         created() {
-            console.log("hellooofwoefoew")
         },
     }
 </script>
-
 <style scoped>
 
 </style>
