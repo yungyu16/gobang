7491b33ce8bb00b6ccd57e1bfdc270b5a6f0483a
yungyu16 <448687557@qq.com>
2019-11-08 20:27:14 +0800

    接口

--- backend/src/main/java/com/github/yungyu16/gobang/web/http/AccountController.java
+++ backend/src/main/java/com/github/yungyu16/gobang/web/http/AccountController.java
@@ -6,6 +6,7 @@ package com.github.yungyu16.gobang.web.http;
 
 import cn.xiaoshidai.common.toolkit.base.StringTools;
 import cn.xiaoshidai.common.toolkit.exception.BizException;
+import com.github.yungyu16.gobang.model.ReqResult;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,17 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("account")
 public class AccountController {
 
-    @RequestMapping("sign-up")
-    public void signUp(@RequestBody String userName) {
+    @RequestMapping("validate")
+    public ReqResult validate(@RequestBody String userName) {
         if (StringTools.isBlank(userName)) {
             throw new BizException("用户名为空");
 
         }
+        return null;
+    }
 
+    @RequestMapping("sign-up")
+    public ReqResult signUp(@RequestBody String userName) {
+        if (StringTools.isBlank(userName)) {
+            throw new BizException("用户名为空");
+        }
+        return null;
     }
 
     @RequestMapping("history")
     public void history() {
     }
-
 }
