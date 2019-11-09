3102df4f9bef638cb52b99ffe46405e535f18995
yungyu16 <448687557@qq.com>
2019-11-10 04:34:50 +0800

    优化

--- backend/src/main/java/com/github/yungyu16/gobang/dao/entity/GameRecord.java
+++ backend/src/main/java/com/github/yungyu16/gobang/dao/entity/GameRecord.java
@@ -1,9 +1,6 @@
 package com.github.yungyu16.gobang.dao.entity;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -27,6 +24,7 @@ public class GameRecord implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @TableId(value = "id", type = IdType.AUTO)
     private Integer id;
 
     private Integer blackUserId;
