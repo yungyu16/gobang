409f7b81b2a1a5d31c864d5e8f92bd95f5937720
yungyu16 <448687557@qq.com>
2019-11-10 02:47:56 +0800

    dao

--- backend/src/main/java/com/github/yungyu16/gobang/web/http/BaseController.java
+++ backend/src/main/java/com/github/yungyu16/gobang/web/http/BaseController.java
@@ -4,17 +4,7 @@
 
 package com.github.yungyu16.gobang.web.http;
 
-import cn.xiaoshidai.common.toolkit.base.ServletTools;
-import cn.xiaoshidai.common.toolkit.exception.BizSessionTimeOutException;
-import com.github.yungyu16.gobang.base.RedisOperationBase;
 import com.github.yungyu16.gobang.base.SessionOperationBase;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.ServletRequestBindingException;
-import org.springframework.web.bind.ServletRequestUtils;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
 
 /**
  * @author Yungyu
@@ -22,20 +12,4 @@ import java.util.Optional;
  */
 public abstract class BaseController extends SessionOperationBase {
 
-    protected Logger log = LoggerFactory.getLogger(getClass());
-
-    protected Optional<String> getCurrentUserToken() {
-        HttpServletRequest currentRequest = ServletTools.getCurrentRequest();
-        String token = null;
-        try {
-            token = ServletRequestUtils.getStringParameter(currentRequest, "token");
-        } catch (ServletRequestBindingException e) {
-            throw new RuntimeException(e);
-        }
-        return Optional.ofNullable(token);
-    }
-
-    protected String getCurrentUserTokenOrException() {
-        return getCurrentUserToken().orElseThrow(BizSessionTimeOutException::new);
-    }
 }
