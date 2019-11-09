a143228995a6a74296b7c60fb528906c70234e34
yungyu16 <448687557@qq.com>
2019-11-10 02:50:02 +0800

    dao

--- backend/src/main/java/com/github/yungyu16/gobang/config/WebSocketConfig.java
+++ backend/src/main/java/com/github/yungyu16/gobang/config/WebSocketConfig.java
@@ -5,6 +5,7 @@
 package com.github.yungyu16.gobang.config;
 
 import com.github.yungyu16.gobang.web.websocket.ChatMsgHandler;
+import com.github.yungyu16.gobang.web.websocket.GameMsgHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -17,10 +18,15 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(gobangMsgHandler(), "ws/game-room").setAllowedOrigins("*");
+        registry.addHandler(chatMsgHandler(), "ws/chat").setAllowedOrigins("*");
+        registry.addHandler(gameMsgHandler(), "ws/game").setAllowedOrigins("*");
     }
 
-    public WebSocketHandler gobangMsgHandler() {
+    public WebSocketHandler chatMsgHandler() {
         return new ChatMsgHandler();
+    }
+
+    public WebSocketHandler gameMsgHandler() {
+        return new GameMsgHandler();
     }
 }
