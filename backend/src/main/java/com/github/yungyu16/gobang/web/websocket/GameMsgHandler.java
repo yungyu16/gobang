ecd94800d121eecd214d920654987f61c09e5d41
yungyu16 <448687557@qq.com>
2019-11-10 02:55:49 +0800

    dao

--- backend/src/main/java/com/github/yungyu16/gobang/web/websocket/GameMsgHandler.java
+++ backend/src/main/java/com/github/yungyu16/gobang/web/websocket/GameMsgHandler.java
@@ -13,6 +13,11 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class GameMsgHandler extends TextWebSocketHandler {
 
     @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        session.sendMessage(new TextMessage("hello world"));
+    }
+
+    @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
         JSONObject json = JSON.parseObject(payload);
--- frontend/src/template/common/basePage.vue
+++ frontend/src/template/common/basePage.vue
@@ -14,7 +14,35 @@
         data() {
             return {};
         },
+        created() {
+            this.initWebSocket();
+        },
         methods: {
+            initWebSocket() {
+                const wsUri = "ws://47.102.103.194:8099/ws/game";
+                this.websock = new WebSocket(wsUri);
+                this.websock.onmessage = this.onWebsocketMessage();
+                this.websock.onopen = this.onWebsocketOpen();
+                this.websock.onerror = this.onWebsocketError();
+                this.websock.onclose = this.onWebsocketClose();
+            },
+            onWebsocketOpen() {
+                console.log("ws open");
+                let actions = {"test": "12345"};
+                this.websocketSend(JSON.stringify(actions));
+            },
+            onWebsocketError() {
+                this.initWebSocket();
+            },
+            onWebsocketMessage(e) {
+                console.log(e)
+            },
+            websocketSend(Data) {
+                this.websock.send(Data);
+            },
+            onWebsocketClose(e) {
+                Notify({type: 'danger', message: '连接中断,请刷新页面'});
+            },
             onClickLeft() {
                 this.$router.go(-1);
             }
